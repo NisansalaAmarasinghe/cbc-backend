7f07f85981b0a8ff05b3e3e162d544347ef7a525
Nisansala <sumudunisansala999@gmail.com>
2025-01-13 23:21:52 +0530

    Correct error with the user 👤

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,11 +70,11 @@ export function loggingUser(req,res){
                         message: "User logged in",
                         token: token,
                         user : {
-                            firstName : firstName,
-                            lastName : lastName,
-                            type : type,
-                            profilePicture : profilePicture,
-                            email : email
+                            firstName : user.firstName,
+                            lastName : user.lastName,
+                            type : user.type,
+                            profilePicture : user.profilePicture,
+                            email : user.email
                         }
                     })
                     
